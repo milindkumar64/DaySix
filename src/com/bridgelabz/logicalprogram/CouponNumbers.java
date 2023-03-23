7d28602e73fcaa6aaeaa7393429b3a68e89726e6
milindkumar64 <milindgupta@gmail.com>
2023-03-24 01:12:09 +0530

    Add: Logical Program Files are added

--- src/com/bridgelabz/logicalprogram/CouponNumbers.java
+++ src/com/bridgelabz/logicalprogram/CouponNumbers.java
@@ -5,20 +5,48 @@ import java.util.Random;
 public class CouponNumbers {
 
 	public static void main(String[] args) {
-		
-		int couponArr[] = {123,127,244,535,355,442,24,25,255,55};
-		int randNum=0;
-		int count=1;
-		Random rn = new Random();
-		for(int i=0;i<couponArr.length;i++)
-		{
-			while(couponArr[i]!=randNum) 
-			{
-		    randNum = rn.nextInt(1000);
-		    count++;
-		    }
-	    }
-		System.out.println("total number of trial to get all Coupon Number is  :"+count);
 
-}
+		String couponArr[] = { "d4B4", "AC7D", "FB2e", "3A82", "6DE9" };
+		String randNum = "0";
+		int count = 1;
+		Random rn = new Random();
+		
+
+		for (int i = 0; i < couponArr.length; i++) {
+			while (!(couponArr[i].equals(randNum)))
+
+			{
+				randNum = getAlphaNumericString();
+				count++;
+			}
+			count++;
+
+		}
+
+		System.out.println("total number of trial to get all Coupon Number is  :" + count);
+
+	}
+	
+
+	static String getAlphaNumericString() {
+
+		String AlphaNumericString = "ABCDEF" + "0123456789" + "abcdef";
+
+		// creating 6 digit StringBuffer
+
+		StringBuilder sb = new StringBuilder(4);
+
+		for (int i = 0; i < 4; i++) {
+
+			// generate a random number between
+			// 0 to AlphaNumericString variable length
+			int index = (int) (AlphaNumericString.length() * Math.random());
+
+			// add Character one by one in end of sb
+			sb.append(AlphaNumericString.charAt(index));
+		}
+		//System.out.println(sb.toString());
+		return sb.toString();
+
+	}
 }
